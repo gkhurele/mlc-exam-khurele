0c1485dfe3cd70deef1f36cd6e886620ad892192
khurele <macbookairm2@192.168.1.8>
2025-08-24 10:18:15 +0800

    test commit to run pipe on jenkins

--- SoftUniBazar.IntegrationTests/AdControllerTests.cs
+++ SoftUniBazar.IntegrationTests/AdControllerTests.cs
@@ -22,7 +22,7 @@ namespace SoftUniBazar.Tests
         [SetUp]
         public void Setup()
         {
-            // Set up in-memory database for testing dev to state
+            // Set up in-memory database for/ test pipe on jenkins
             var options = new DbContextOptionsBuilder<BazarDbContext>()
                 .UseInMemoryDatabase(databaseName: "BazarDbTest")
                 .Options;
